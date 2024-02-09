91493fd51cef1f4749b7c390d25524d53bae3c75
saltchicken <john.eicher89@gmail.com>
2024-02-08 21:41:04 -0800

    Use updated generate_fm_packet. Refactor needed.

--- uhd_tx.py
+++ uhd_tx.py
@@ -11,12 +11,18 @@ def setup():
 if __name__ == "__main__":
     sample_rate, center_freq, streamer = setup()
 
+    # freq = 50000
+    # freq_deviation = 10000
+    # duration = 1
+    # symbol_size = 250000
+
+    # transmission_signal = utils.generate_fm_packet(streamer, '1010001', freq, freq_deviation, symbol_size / sample_rate)
+    
     freq = 50000
     freq_deviation = 10000
-    duration = 1
-    symbol_size = 250000
+    duration = 3
 
-    transmission_signal = utils.generate_fm_packet(streamer, '1010001', freq, freq_deviation, symbol_size / sample_rate)
+    transmission_signal = utils.generate_fm_packet_update('10100010', freq - freq_deviation, freq + freq_deviation, duration, sample_rate)
 
     # plt.plot(transmission_signal, label="Transmission")
     # plt.xlabel("Index")
