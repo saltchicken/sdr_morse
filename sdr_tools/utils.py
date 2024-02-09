1640a910b6bf96b8f2997d31e59a8d5c1574cba8
saltchicken <john.eicher89@gmail.com>
2024-02-08 21:25:41 -0800

    Add updated fm_packet function to utils for testing remotely.

--- sdr_tools/utils.py
+++ sdr_tools/utils.py
@@ -18,6 +18,37 @@ def generate_fm_packet(streamer, binary_string, freq, freq_deviation, duration):
             print('error')
     return transmission_signal
 
+def generate_fm_packet_update(binary_string, frequency, second_frequency, duration, sample_rate):
+    t = np.arange(0, duration, 1 / sample_rate)
+    num_symbols = len(binary_string)
+    symbol_length = len(t) / num_symbols
+    assert int(symbol_length) == symbol_length, "Sample amount of t must be divisible by num_symbols"
+    symbol_length = int(symbol_length)
+    print("Num symbols: ", num_symbols, "|", "Symbol length: ", symbol_length)
+    transmission_signal = np.zeros(len(t))
+    time_interval = 1 / sample_rate
+    
+    for i, bit in enumerate(binary_string):
+        start_index = symbol_length * i
+        end_index = symbol_length * i + symbol_length
+        symbol_time = symbol_length * time_interval
+        if i == 0:
+            phase_shift = 0.0
+        elif binary_string[i-1] == '0':
+            phase_shift += 2 * np.pi * frequency * symbol_time
+        elif binary_string[i-1] == '1':
+            phase_shift += 2 * np.pi * second_frequency * symbol_time
+        else:
+            print("Something is wrong with calculating phase shift")
+        if bit == '0':
+            symbol_wave = np.cos(2 * np.pi * frequency * t + phase_shift)
+        elif bit == '1':
+            symbol_wave = np.cos(2 * np.pi * second_frequency * t + phase_shift)
+        else:
+            print("Something is wrong with the binary_string")
+        transmission_signal[start_index:end_index] = symbol_wave[0:symbol_length]
+    return transmission_signal
+
 def cos_wave_generator(sample_rate, frequency, samples):
     i = 0
     while True:
