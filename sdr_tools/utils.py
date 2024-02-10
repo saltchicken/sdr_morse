20442f75dc6c54460c11520238fadabf0d55b09a
saltchicken <john.eicher89@gmail.com>
2024-02-10 13:59:22 -0800

    Refactor generate_fm_packet for complex number usage. Legacy functions removed.

--- sdr_tools/utils.py
+++ sdr_tools/utils.py
@@ -5,51 +5,7 @@ def generate_carrier(streamer, frequency, duration):
     carrier_signal = np.cos(2 * np.pi * frequency * t)
     return carrier_signal
 
-def generate_fm_packet_legacy(streamer, binary_string, freq, freq_deviation, duration):
-    carrier_signal = generate_carrier(streamer, freq - freq_deviation // 2, duration)
-    deviation_signal = generate_carrier(streamer, freq + freq_deviation // 2, duration)
-    transmission_signal = np.tile(carrier_signal, 8)
-    for char in binary_string:
-        if char == '0':
-            transmission_signal = np.append(transmission_signal, carrier_signal)
-        elif char == '1':
-            transmission_signal = np.append(transmission_signal, deviation_signal)
-        else:
-            print('error')
-    return transmission_signal
-
-def generate_fm_packet_update(binary_string, frequency, second_frequency, duration, sample_rate):
-    t = np.arange(0, duration, 1 / sample_rate)
-    num_symbols = len(binary_string)
-    symbol_length = len(t) / num_symbols
-    assert int(symbol_length) == symbol_length, "Sample amount of t must be divisible by num_symbols"
-    symbol_length = int(symbol_length)
-    print("Num symbols: ", num_symbols, "|", "Symbol length: ", symbol_length)
-    transmission_signal = np.zeros(len(t))
-    time_interval = 1 / sample_rate
-    
-    for i, bit in enumerate(binary_string):
-        start_index = symbol_length * i
-        end_index = symbol_length * i + symbol_length
-        symbol_time = symbol_length * time_interval
-        if i == 0:
-            phase_shift = 0.0
-        elif binary_string[i-1] == '0':
-            phase_shift += 2 * np.pi * frequency * symbol_time
-        elif binary_string[i-1] == '1':
-            phase_shift += 2 * np.pi * second_frequency * symbol_time
-        else:
-            print("Something is wrong with calculating phase shift")
-        if bit == '0':
-            symbol_wave = np.cos(2 * np.pi * frequency * t + phase_shift)
-        elif bit == '1':
-            symbol_wave = np.cos(2 * np.pi * second_frequency * t + phase_shift)
-        else:
-            print("Something is wrong with the binary_string")
-        transmission_signal[start_index:end_index] = symbol_wave[0:symbol_length]
-    return transmission_signal
-
-def generate_fm_packet_complex(binary_string, frequency, second_frequency, duration, sample_rate):
+def generate_fm_packet(binary_string, frequency, second_frequency, duration, sample_rate):
     t = np.arange(0, duration, 1 / sample_rate)
     num_symbols = len(binary_string)
     symbol_length = len(t) / num_symbols
--- uhd_tx.py
+++ uhd_tx.py
@@ -1,8 +1,6 @@
 from sdr_tools import classes
 from sdr_tools import utils
 
-import time
-
 def setup():
     sample_rate = 1e6
     center_freq = 434e6
@@ -11,36 +9,11 @@ def setup():
 
 if __name__ == "__main__":
     sample_rate, center_freq, streamer = setup()
-
-    # freq = 50000
-    # freq_deviation = 10000
-    # duration = 1
-    # symbol_size = 250000
-
-    # transmission_signal = utils.generate_fm_packet(streamer, '1010001', freq, freq_deviation, symbol_size / sample_rate)
     
     freq = 40000
     freq_deviation = 10000
     duration = 0.5
 
-    transmission_signal = utils.generate_fm_packet_update('10100010', freq - freq_deviation, freq + freq_deviation, duration, sample_rate)
+    transmission_signal = utils.generate_fm_packet('10100010', freq - freq_deviation, freq + freq_deviation, duration, sample_rate)
 
-    complex_transmission_signal = utils.generate_fm_packet_complex('10100010', freq - freq_deviation, freq + freq_deviation, duration, sample_rate)
-
-    # carrier_signal = utils.generate_carrier(streamer, 50000, duration)
-
-    # modulated_signal = carrier_signal * transmission_signal
-
-    # plt.plot(transmission_signal, label="Transmission")
-    # plt.xlabel("Index")
-    # plt.ylabel("Amplitude")
-    # plt.legend()
-    # plt.show()
-
-
-    # streamer.send(transmission_signal)
-    # time.sleep(0.25)
-    streamer.send(complex_transmission_signal)
-    # streamer.send(modulated_signal)
-    # time.sleep(0.25)
-    # streamer.send(deviation_signal)
+    streamer.send(transmission_signal)
