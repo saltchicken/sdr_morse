dee1120196444418eea3ec4e25baeb218ca1ad07
saltchicken <john.eicher89@gmail.com>
2024-02-10 13:48:29 -0800

    Update TX and RX with new runtimes from testing.

--- lime_rx.py
+++ lime_rx.py
@@ -1,5 +1,9 @@
 from sdr_tools import classes
 from sdr_tools import utils
+import numpy as np
+
+from scipy.signal import resample_poly, firwin, bilinear, lfilter
+import matplotlib.pyplot as plt
 
 # apply settings
 sample_rate = 2e6
@@ -10,15 +14,25 @@ buffer_size = 10000000
 
 receiver = classes.Receiver(sample_rate, frequency, antenna, buffer_size)
 
-frequency = -500000  # Adjust the frequency as needed
+frequency = -540000  # Adjust the frequency as needed
 wave_gen = utils.cos_wave_generator(sample_rate, frequency, buffer_size)
 
 received = receiver.read()
 
 modulated = received * next(wave_gen)
 
+# demodulated = 0.5 * np.angle(modulated[0:-1] * np.conj(modulated[1:]))
+
+# taps = firwin(numtaps=101, cutoff=150e3, fs=sample_rate)
+# modulated = np.convolve(modulated, taps, 'valid')
+
+# Decimate by 10
+# modulated = modulated[::20]
+# plt.plot(filtered)
+# plt.show()
+
 output_file = 'samples.bin'
+print(modulated.shape)
 modulated.tofile(output_file)
 
-print('closing time')
 receiver.close()
--- uhd_tx.py
+++ uhd_tx.py
@@ -1,6 +1,8 @@
 from sdr_tools import classes
 from sdr_tools import utils
 
+import time
+
 def setup():
     sample_rate = 1e6
     center_freq = 434e6
@@ -17,11 +19,17 @@ if __name__ == "__main__":
 
     # transmission_signal = utils.generate_fm_packet(streamer, '1010001', freq, freq_deviation, symbol_size / sample_rate)
     
-    freq = 50000
+    freq = 40000
     freq_deviation = 10000
-    duration = 3
+    duration = 0.5
 
     transmission_signal = utils.generate_fm_packet_update('10100010', freq - freq_deviation, freq + freq_deviation, duration, sample_rate)
+
+    complex_transmission_signal = utils.generate_fm_packet_complex('10100010', freq - freq_deviation, freq + freq_deviation, duration, sample_rate)
+
+    # carrier_signal = utils.generate_carrier(streamer, 50000, duration)
+
+    # modulated_signal = carrier_signal * transmission_signal
 
     # plt.plot(transmission_signal, label="Transmission")
     # plt.xlabel("Index")
@@ -30,6 +38,9 @@ if __name__ == "__main__":
     # plt.show()
 
 
-    streamer.send(transmission_signal)
+    # streamer.send(transmission_signal)
+    # time.sleep(0.25)
+    streamer.send(complex_transmission_signal)
+    # streamer.send(modulated_signal)
     # time.sleep(0.25)
     # streamer.send(deviation_signal)
