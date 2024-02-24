3fa85a3a79cfaa3708f8b8cd3f5538beefcde180
saltchicken <john.eicher89@gmail.com>
2024-02-24 10:33:23 -0800

    Refactor

--- uhd_tx.py
+++ uhd_tx.py
@@ -1,19 +1,18 @@
-from sdr_tools import classes
-from sdr_tools import utils
+from sdr_tools import UHD_TX_Streamer
 
 from IPython import embed
 
 if __name__ == "__main__":  
     sample_rate = 2e6
     center_freq = 434e6
-    streamer = classes.UHD_TX_Streamer(sample_rate, center_freq)
+    transmitter = UHD_TX_Streamer(sample_rate, center_freq)
     
     freq = 40000
     freq_deviation = 10000
     duration = 0.5
 
-    fm_packet = streamer.generate_fm_packet('10100010', freq, freq_deviation, duration)
-    bpsk = streamer.generateBPSK('10100010')
+    fm_packet = transmitter.generate_fm_packet('10100010', freq, freq_deviation, duration)
+    # bpsk = transmitter.generateBPSK('10100010')
 
     # streamer.send(transmission_signal)
     
