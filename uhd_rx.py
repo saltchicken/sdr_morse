d06211c917b7fcd38983dd424a2a39f392c5e62c
saltchicken <john.eicher89@gmail.com>
2024-02-25 15:43:53 -0800

    Add init variables to uhd_rx testing

--- uhd_rx.py
+++ uhd_rx.py
@@ -2,6 +2,9 @@ from sdr_tools.classes import UHD_RX
 
 from IPython import embed
 
+sample_rate = 2e6
+frequency = 434e6
+antenna = 'LNAW'
 
-receiver = UHD_RX() 
+receiver = UHD_RX(sample_rate, frequency, antenna) 
 embed()
