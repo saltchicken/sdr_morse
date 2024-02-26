f7fcf015f00cadb975ae250ddb2fd16d86ab4414
saltchicken <john.eicher89@gmail.com>
2024-02-26 15:58:31 -0800

    receiver to transceiver for testing

--- lime_rx_tx.py
+++ lime_rx_tx.py
@@ -9,5 +9,10 @@ antenna = 'LNAW'
 
 tx_freq = 434e6 # center_freq
 
-with Lime_RX_TX(sample_rate, rx_freq, tx_freq, antenna, 'BAND2') as receiver:
+with Lime_RX_TX(sample_rate, rx_freq, tx_freq, antenna, 'BAND2') as transceiver:
+    freq = 40000
+    freq_deviation = 10000
+    symbol_length = 10000
+
+    fm_packet = transceiver.generate_fm_packet('10100010', freq, freq_deviation, symbol_length)
     embed()
