844889117e9a54f21eb217d2f2d27e04c39231b4
saltchicken <john.eicher89@gmail.com>
2024-02-26 17:38:54 -0800

    Use with statement for transmitter

--- uhd_tx.py
+++ uhd_tx.py
@@ -6,16 +6,16 @@ if __name__ == "__main__":
     sample_rate = 2e6
     center_freq = 434e6
     antenna = ''
-    transmitter = UHD_TX(sample_rate, center_freq, antenna)
+    with UHD_TX(sample_rate, center_freq, antenna) as transmitter:
     
-    freq = 40000
-    freq_deviation = 10000
-    symbol_length = 10000
+        freq = 40000
+        freq_deviation = 10000
+        symbol_length = 10000
 
-    fm_packet = transmitter.generate_fm_packet('10100010', freq, freq_deviation, symbol_length)
-    # bpsk = transmitter.generateBPSK('10100010')
+        fm_packet = transmitter.generate_fm_packet('10100010', freq, freq_deviation, symbol_length)
+        # bpsk = transmitter.generateBPSK('10100010')
 
-    # streamer.send(transmission_signal)
-    
-    transmitter.set_gain(20)
-    embed()
+        # streamer.send(transmission_signal)
+        
+        transmitter.set_gain(20)
+        embed()
