5c700b303bd514fde39bc1c90f9d2c54f4c57c1c
saltchicken <john.eicher89@gmail.com>
2024-03-02 11:09:05 -0800

    Returning to tried and true rx_stream.recv

--- sdr_tools/classes.py
+++ sdr_tools/classes.py
@@ -531,8 +531,8 @@ class UHD_RX(Receiver):
         #     self.rx_streamer.recv(recv_buffer, metadata)
         #     samples[i*1000:(i+1)*1000] = recv_buffer[0]
         
-        # self.rx_streamer.recv(self.read_buffer, self.rx_metadata)
-        self.read_buffer = np.copy(self.usrp.recv_num_samps(2000, self.frequency, self.sample_rate, [0], 0))
+        self.rx_streamer.recv(self.read_buffer, self.rx_metadata)
+        # self.read_buffer = np.copy(self.usrp.recv_num_samps(2000, self.frequency, self.sample_rate, [0], 0))
         # stream_cmd = uhd.types.StreamCMD(uhd.types.StreamMode.stop_cont)
         # self.rx_streamer.issue_stream_cmd(stream_cmd)
         return self.read_buffer
