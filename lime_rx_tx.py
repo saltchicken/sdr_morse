bc61b6dbc59ce177d59c581086f7b0c717c5b4f5
saltchicken <john.eicher89@gmail.com>
2024-03-01 18:40:16 -0800

    fm_packet needed for initial send

--- lime_rx_tx.py
+++ lime_rx_tx.py
@@ -13,5 +13,5 @@ tx_channel = 40000
 tx_freq = 434e6 # center_freq
 
 with Lime_RX_TX(sample_rate, rx_freq, tx_freq, antenna, 'BAND2', rx_channel, tx_channel, full_duplex=True) as transceiver:
-    # fm_packet = FM_Packet('101000111001001000010100101101010110', channel_freq=40000)
+    fm_packet = FM_Packet('101000111001001000010100101101010110', channel_freq=40000)
     embed()
